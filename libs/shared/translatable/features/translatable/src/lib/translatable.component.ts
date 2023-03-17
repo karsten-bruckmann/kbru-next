29f8858f553b80438ce4ed8c2f43c67d7efcc37b
Karsten Bruckmann <karsten.bruckmann@check24.de>
2023-03-17 23:14:25 +0100

    [main] UI

--- libs/shared/translatable/features/translatable/src/lib/translatable.component.ts
+++ libs/shared/translatable/features/translatable/src/lib/translatable.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, HostListener, Input } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { IonicModule, ModalController } from '@ionic/angular';
 import {
   TranslationManagementModule,
   translationSelector,
 } from '@kbru/shared/translatable/core/translation-management';
 import { Store } from '@ngrx/store';
-import { BehaviorSubject, switchMap } from 'rxjs';
+import { BehaviorSubject, map, switchMap } from 'rxjs';
 
 import { EditorComponent } from './editor/editor.component';
 
@@ -20,7 +21,8 @@ import { EditorComponent } from './editor/editor.component';
 export class TranslatableComponent {
   constructor(
     private modalController: ModalController,
-    private store$: Store
+    private store$: Store,
+    private domSanitizer: DomSanitizer
   ) {}
 
   @Input() public set text(text: string) {
@@ -32,7 +34,8 @@ export class TranslatableComponent {
   protected text$ = new BehaviorSubject<string>('');
 
   protected translation$ = this.text$.pipe(
-    switchMap((text) => this.store$.select(translationSelector(text)))
+    switchMap((text) => this.store$.select(translationSelector(text))),
+    map((text) => this.listify(text))
   );
 
   @HostListener('click')
@@ -49,5 +52,21 @@ export class TranslatableComponent {
     });
 
     modal.present();
+  }
+
+  public listify(text: string): SafeHtml {
+    let beautified = text.replaceAll('<', '&lt;').replaceAll('>', '&gt;');
+    [...text.matchAll(/(?:(?:^|\n)(?:- [^\n]+(?:\n|$)*)+)/gm)].forEach(
+      (match) => {
+        beautified = beautified.replace(
+          match[0],
+          `<ul>${match[0].replaceAll(
+            /(?:^|\n)- (.*)(?:\n|$)/gm,
+            '<li>$1</li>'
+          )}</ul>`
+        );
+      }
+    );
+    return this.domSanitizer.bypassSecurityTrustHtml(beautified);
   }
 }
