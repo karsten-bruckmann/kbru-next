14a4b29db3653f426465c85f6095dc1ec350bc75
Karsten Bruckmann <karsten.bruckmann@check24.de>
2023-03-25 12:49:46 +0100

    [main] try messaging

--- apps/battle-companion/src/app/app.component.ts
+++ apps/battle-companion/src/app/app.component.ts
@@ -1,7 +1,13 @@
-import { Component, EnvironmentInjector, Optional } from '@angular/core';
+import {
+  Component,
+  EnvironmentInjector,
+  OnInit,
+  Optional,
+} from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
 import { IonicModule } from '@ionic/angular';
+import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 import { filter } from 'rxjs';
 
 @Component({
@@ -11,7 +17,7 @@ import { filter } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(
     public environmentInjector: EnvironmentInjector,
     @Optional() updates?: SwUpdate
@@ -25,5 +31,28 @@ export class AppComponent {
           updates.activateUpdate().then(() => document.location.reload());
         }
       });
+  }
+
+  public ngOnInit(): void {
+    const messaging = getMessaging();
+    getToken(messaging, {
+      vapidKey:
+        'BBWfj0eWNkhU0At_zun0udQRshWjipxjqlGp7aOdjR19P1mbAze1wbNHWII5KY5xwwN4w-6qj7tKqx0wt5RzulM',
+    })
+      .then((currentToken) => {
+        if (currentToken) {
+          alert(currentToken);
+        } else {
+          console.log(
+            'No registration token available. Request permission to generate one.'
+          );
+        }
+      })
+      .catch((err) => {
+        console.error('An error occurred while retrieving token. ', err);
+      });
+    onMessage(messaging, (payload) => {
+      alert(JSON.stringify(payload));
+    });
   }
 }
