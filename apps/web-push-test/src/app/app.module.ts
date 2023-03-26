919cc264714c565f7b70186c4ab09630aa7d1827
Karsten Bruckmann <karsten.bruckmann@check24.de>
2023-03-26 12:35:03 +0200

    [main] Clean up

--- apps/web-push-test/src/app/app.module.ts
+++ apps/web-push-test/src/app/app.module.ts
@@ -25,12 +25,6 @@ initializeApp({
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000',
     }),
-    ServiceWorkerModule.register('firebase-messaging-sw.js', {
-      enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000',
-    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
