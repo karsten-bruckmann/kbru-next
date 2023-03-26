1308e71d4397157d00f37fd6efc99846fa4108e7
Karsten Bruckmann <karsten.bruckmann@check24.de>
2023-03-26 22:15:31 +0200

    [main] register background notifications

--- apps/web-push-test/src/firebase-messaging-sw.js
+++ apps/web-push-test/src/firebase-messaging-sw.js
@@ -15,3 +15,22 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log(
+    '[firebase-messaging-sw.js] Received background message ',
+    payload
+  );
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+  };
+  return self.registration.showNotification(
+    notificationTitle,
+    notificationOptions
+  );
+});
+
+self.addEventListener('notificationclick', (event) => {
+  console.log(event);
+});
