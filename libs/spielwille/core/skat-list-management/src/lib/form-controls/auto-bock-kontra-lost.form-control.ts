5a6fe7e454c5e3aeb006ebaa2df79e463a39e6d7
Karsten Bruckmann <karsten.bruckmann@gmail.com>
2023-04-21 15:03:18 +0200

    [main] All controls visible

--- libs/spielwille/core/skat-list-management/src/lib/form-controls/auto-bock-kontra-lost.form-control.ts
+++ libs/spielwille/core/skat-list-management/src/lib/form-controls/auto-bock-kontra-lost.form-control.ts
@@ -1,6 +1,7 @@
 import { FormControl, ValidatorFn } from '@angular/forms';
 import { FormEffect } from '@kbru/shared/utils/effect-aware-forms';
-import { map, startWith } from 'rxjs';
+import { toVoid } from '@kbru/shared/utils/rxjs-utils';
+import { combineLatest, startWith, tap } from 'rxjs';
 
 import { SkatListFormGroup } from '../form-groups/skat-list.form-group';
 import { requiredBooleanValidatorFunction } from '../validator-functions/required-boolean.validator-function';
@@ -12,18 +13,28 @@ export class AutoBockKontraLostFormControl extends FormControl<boolean | null> {
 
   public static formEffect(): FormEffect<SkatListFormGroup> {
     return (form) => {
-      return form.controls.bockSets.valueChanges.pipe(
-        startWith(form.controls.bockSets.value),
-        map((bockSets) => {
-          if (bockSets && form.controls.autoBockKontraLost.disabled) {
+      return combineLatest([
+        form.controls.bockSets.valueChanges.pipe(
+          startWith(form.controls.bockSets.value)
+        ),
+        form.controls.kontra.valueChanges.pipe(
+          startWith(form.controls.kontra.value)
+        ),
+      ]).pipe(
+        tap(([bockSets, kontra]) => {
+          if (bockSets && kontra && form.controls.autoBockKontraLost.disabled) {
             form.controls.autoBockKontraLost.enable();
             form.controls.autoBockKontraLost.setValue(true);
           }
-          if (!bockSets && form.controls.autoBockKontraLost.enabled) {
+          if (
+            (!bockSets || !kontra) &&
+            form.controls.autoBockKontraLost.enabled
+          ) {
             form.controls.autoBockKontraLost.disable();
             form.controls.autoBockKontraLost.setValue(false);
           }
-        })
+        }),
+        toVoid()
       );
     };
   }
--- libs/spielwille/core/skat-list-management/src/lib/form-controls/auto-bock-kontra-re.form-control.ts
+++ libs/spielwille/core/skat-list-management/src/lib/form-controls/auto-bock-kontra-re.form-control.ts
@@ -1,6 +1,6 @@
 import { FormControl, ValidatorFn } from '@angular/forms';
 import { FormEffect } from '@kbru/shared/utils/effect-aware-forms';
-import { map, startWith } from 'rxjs';
+import { combineLatest, map, startWith } from 'rxjs';
 
 import { SkatListFormGroup } from '../form-groups/skat-list.form-group';
 import { requiredBooleanValidatorFunction } from '../validator-functions/required-boolean.validator-function';
@@ -12,14 +12,29 @@ export class AutoBockKontraReFormControl extends FormControl<boolean | null> {
 
   public static formEffect(): FormEffect<SkatListFormGroup> {
     return (form) =>
-      form.controls.bockSets.valueChanges.pipe(
-        startWith(form.controls.bockSets.value),
-        map((bockSets) => {
-          if (bockSets && form.controls.autoBockKontraRe.disabled) {
+      combineLatest([
+        form.controls.bockSets.valueChanges.pipe(
+          startWith(form.controls.bockSets.value)
+        ),
+        form.controls.kontra.valueChanges.pipe(
+          startWith(form.controls.kontra.value)
+        ),
+        form.controls.re.valueChanges.pipe(startWith(form.controls.re.value)),
+      ]).pipe(
+        map(([bockSets, kontra, re]) => {
+          if (
+            bockSets &&
+            kontra &&
+            re &&
+            form.controls.autoBockKontraRe.disabled
+          ) {
             form.controls.autoBockKontraRe.enable();
             form.controls.autoBockKontraRe.setValue(true);
           }
-          if (!bockSets && form.controls.autoBockKontraRe.enabled) {
+          if (
+            (!bockSets || !kontra || !re) &&
+            form.controls.autoBockKontraRe.enabled
+          ) {
             form.controls.autoBockKontraRe.disable();
             form.controls.autoBockKontraRe.setValue(false);
           }
