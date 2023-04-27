723362107cfae688bed70dcc15ce3a5d54fe7e3d
Karsten Bruckmann <karsten-bruckmann@users.noreply.github.com>
2023-04-27 22:43:15 +0200

    [main] Always allow announcements without hand in romanow

--- libs/spielwille/core/skat-list-management/src/lib/form-controls/threshold-announcement-without-hand.form-control.ts
+++ libs/spielwille/core/skat-list-management/src/lib/form-controls/threshold-announcement-without-hand.form-control.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { FormEffect } from '@kbru/shared/utils/effect-aware-forms';
-import { EMPTY } from 'rxjs';
+import { toVoid } from '@kbru/shared/utils/rxjs-utils';
+import { startWith, tap } from 'rxjs';
 
 import { SkatListFormGroup } from '../form-groups/skat-list.form-group';
 import { requiredBooleanValidatorFunction } from '../validator-functions/required-boolean.validator-function';
@@ -17,7 +18,17 @@ export class ThresholdAnnouncementWithoutHandControl extends FormControl<
   public formEffect(): FormEffect<SkatListFormGroup> {
     return (form) => {
       form.controls.thresholdAnnouncementWithoutHand.setValue(false);
-      return EMPTY;
+
+      return form.controls.addOn.valueChanges.pipe(
+        startWith(form.controls.addOn.value),
+        tap((addOn) => {
+          if (addOn === 'romanow') {
+            this.setValue(true);
+            this.disable();
+          }
+        }),
+        toVoid()
+      );
     };
   }
 }
