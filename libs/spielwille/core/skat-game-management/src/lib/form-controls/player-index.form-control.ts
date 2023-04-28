66819c76cd722515d6614b8ce271a4b870911349
Karsten Bruckmann <karsten-bruckmann@users.noreply.github.com>
2023-04-28 14:01:53 +0200

    [main] - Auto select romanow player - Allow only players that have game types available

--- libs/spielwille/core/skat-game-management/src/lib/form-controls/player-index.form-control.ts
+++ libs/spielwille/core/skat-game-management/src/lib/form-controls/player-index.form-control.ts
@@ -1,7 +1,7 @@
 import { FormControl } from '@angular/forms';
 import { FormEffect } from '@kbru/shared/utils/effect-aware-forms';
 import { toVoid } from '@kbru/shared/utils/rxjs-utils';
-import { Observable, tap } from 'rxjs';
+import { combineLatest, delay, Observable, startWith, tap } from 'rxjs';
 
 import { SkatGameFormGroup } from '../form-groups/skat-game.form-group';
 import { List } from '../models/list.model';
@@ -17,11 +17,18 @@ export class PlayerIndexFormControl extends FormControl<number | null> {
     list$: Observable<List | null>
   ): FormEffect<SkatGameFormGroup> {
     return (form) => {
-      return list$.pipe(
-        tap((list) => {
+      return combineLatest([
+        list$,
+        this.valueChanges.pipe(startWith(this.value)),
+      ]).pipe(
+        delay(1),
+        tap(([list]) => {
           form.controls.playerIndex.possibleValues =
-            list?.status?.activePlayers || [];
-          if (list?.rules.addOn === 'romanow') {
+            list?.status?.activePlayers.filter(
+              (playerIndex) =>
+                list.status.availableGameTypes[playerIndex].length > 0
+            ) || [];
+          if (list?.rules.addOn === 'romanow' && !form.value.playerIndex) {
             form.controls.playerIndex.setValue(
               form.controls.playerIndex.possibleValues[0]
             );
